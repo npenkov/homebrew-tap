6d664580244a9fb814dbe704737425d6d8b5b622
Nick Penkov <nick.penkov@gmail.com>
2026-02-07 20:50:29 +0100

    fix: bump git-worktree-refresh to v0.2.1, add man page install

--- Formula/git-worktree-refresh.rb
+++ Formula/git-worktree-refresh.rb
@@ -1,14 +1,16 @@
 class GitWorktreeRefresh < Formula
   desc "Scan directories for git repos, fetch remotes in parallel, show worktree status"
   homepage "https://github.com/npenkov/git-worktree-refresh"
-  url "https://github.com/npenkov/git-worktree-refresh/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "cbadb1167fd7c9976bc8c8b83c68c1c25ba22d7afd6dd2a5c700cdac66082552"
+  url "https://github.com/npenkov/git-worktree-refresh/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "f4745c639b03074698d2e4ea59b1ab9a005bd9b5a959f925ab28828e0cab683e"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+    out_dir = Dir["target/release/build/git-worktree-refresh-*/out"].first
+    man1.install "#{out_dir}/git-worktree-refresh.1"
   end
 
   test do
